21a1e0219dc1f3faae00ca1bb2f2bbbaad96f924
skyseanebula <1073430593@qq.com>
2025-07-24 07:37:48 +0800

    使用parchment补充官方映射

--- src/main/java/com/skynebula/strinovamc/StrinovaMc.java
+++ src/main/java/com/skynebula/strinovamc/StrinovaMc.java
@@ -77,7 +77,7 @@ public class StrinovaMc
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
